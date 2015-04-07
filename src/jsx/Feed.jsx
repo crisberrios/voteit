574aa88d65e6187e668e76a6da467c81c53898d2
Cristián Berríos <cristian.berrios@outlook.cl>
2015-04-07 01:46:40 -0500

    Refactored to use Firebase

--- src/jsx/Feed.jsx
+++ src/jsx/Feed.jsx
@@ -3,17 +3,31 @@ var ShowAddButton = require('./ShowAddButton.jsx');
 var FeedForm      = require('./FeedForm.jsx');
 var FeedList      = require('./FeedList.jsx');
 var _             = require('lodash');
+var Firebase      = require('firebase');
+
 
 var Feed = React.createClass({
   displayName: 'Feed',
+  loadData: function() {
+    var fb = new Firebase('https://blazing-fire-230.firebaseio.com/feed');
+    fb.on('value',function(snapshot) {
+      var items = [];
+      snapshot.forEach(function(snap) {
+        var item = snap.val();
+        item.key = snap.key();
+        items.push(item);
+      });
+      this.setState({
+        items: items
+      });
+    }.bind(this));
+  },
+  componentDidMount: function() {
+   this.loadData();
+  },
   getInitialState: function() {
-    var FEED_ITEMS = [
-      { key: '1', title: 'Realtime data!', description: 'Firebase is cool', voteCount: 49 },
-      { key: '2', title: 'JavaScript is fun', description: 'Lexical scoping FTW', voteCount: 34},
-      { key: '3', title: 'Coffee makes you awake', description: 'Drink responsibly', voteCount: 15}
-    ];
     return {
-      items: FEED_ITEMS,
+      items: [],
       formDisplayed: false
     }
   },
@@ -23,23 +37,12 @@ var Feed = React.createClass({
     });
   },
   onNewItem: function(newItem) {
-  this.setState({
-    items: this.state.items.concat([newItem]),
-    formDisplayed: false,
-    key: this.state.items.length
-  });
+    var fb = new Firebase('https://blazing-fire-230.firebaseio.com/feed');
+    fb.push(newItem);
   },
   onVote: function(item) {
-    var items = _.uniq(this.state.items);
-    var index = _.findIndex(items,function(feedItem) {
-      return item.key === feedItem.key;
-    });
-    var oldObj = items[index];
-    var newItems = _.pull(items,oldObj);
-    newItems.push(item);
-    this.setState({
-      items: newItems
-    });
+    var fb = new Firebase('https://blazing-fire-230.firebaseio.com/feed').child(item.key);
+    fb.update(item);
   },
   render: function() {
     return (
